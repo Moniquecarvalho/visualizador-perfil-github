d56dd16c001e3c44dfb74efd14dd80bb1b517081
Monique Carvalho <moniquecarvalho654@gmail.com>
2026-01-13 18:05:33 -0300

    feat: Adiciona lista dos 10 últimos repositórios

--- src/js/profileView.js
+++ src/js/profileView.js
@@ -1,4 +1,25 @@
-export function renderProfile(userData, container) {
+export function renderProfile(userData, userRepos, container) {
+  const repositoriesHTML =
+    userRepos && userRepos.length > 0
+      ? userRepos
+          .map(
+            (repo) => `
+    <div class="repository-card">
+    <a href="${repo.html_url}" target="_blank"
+        <h3>${repo.name}</h3>
+        <div class="repository-stats">
+          <span>⭐ Stars: ${repo.stargazers_count}</span>
+          <span>🍴 Forks: ${repo.forks_count}</span>
+          <span>👀 Watchers: ${repo.watchers_count}</span>
+          <span> 💻 Language: ${repo.language || "Não especificado"}</span>
+        </div>
+    </a>
+    </div>
+  `
+          )
+          .join("")
+      : `<p>Este usuário não possui repositórios públicos.</p>`;
+
   container.innerHTML = `
     <div class="profile-card">
       <img src="${userData.avatar_url}" alt="Avatar de ${
@@ -7,6 +28,7 @@ export function renderProfile(userData, container) {
       <div class="profile-info">
         <h2>${userData.name}</h2>
         <p>${userData.bio || "Não possui bio cadastrada 😢."}</p>
+      </div>
       </div>
     </div>
 
@@ -19,6 +41,13 @@ export function renderProfile(userData, container) {
             <h4>👥 Seguindo</h4>
             <span>${userData.following}</span>
         </div>
+    </div>
+
+    <div class="profile-repositories">
+      <h2>Repositórios</h2>
+      <div class="repositories">
+        ${repositoriesHTML}
+      </div>
     </div>
   `;
 }
